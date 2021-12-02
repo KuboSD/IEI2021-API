d3bda8c1e4f0c613750b7a04ace95eab7c8d75be
JaviRTB <javierrdelatorre@gmail.com>
2021-12-02 22:52:39 +0100

    Avoid duplicates

--- extractores/JSONtoSQL.js
+++ extractores/JSONtoSQL.js
@@ -4,18 +4,26 @@ const json = require("../Fuentes de datos/Euskadi/bibliotecas.json")
 function readJson(){
     let nombre, tipo, direccion, codigopostal, longitud, latitud, telefono, email, descripcion;
     let query = [];
+    let isDuplicated;
     for(let i = 0; i<json.length; i++){
-        nombre = "'" + json[i].documentName+"'";
-        tipo = "'" + json[i].documentName+"'";
-        direccion = "'" + json[i].address+"'";
-        codigopostal = "'" + json[i].postalcode+"'";
-        longitud = "'" + json[i].lonwgs84+"'";
-        latitud = "'" + json[i].latwgs84+"'";
-        telefono = "'" + json[i].phone+"'";
-        email = "'" + json[i].email+"'";
-        descripcion = "'" + json[i].documentDescription+"'";
-        query[i] = "INSERT INTO biblioteca (nombre, tipo, direccion, codigoPostal, longitud, latitud, telefono, email, descripcion, enLocalidad) VALUES(" +
-        nombre + "," + tipo + "," + direccion + "," + codigopostal + "," + longitud + "," + latitud + "," + telefono + "," + email + "," + descripcion + ", NULL);"
+        for(let j = 0; j<i; j++){
+            if(json[i].documentName == json[j].documentName){
+                isDuplicated = true;
+            }
+        }
+        if(!isDuplicated){
+            nombre = "'" + json[i].documentName+"'";
+            tipo = "'" + json[i].documentName+"'";
+            direccion = "'" + json[i].address+"'";
+            codigopostal = "'" + json[i].postalcode+"'";
+            longitud = "'" + json[i].lonwgs84+"'";
+            latitud = "'" + json[i].latwgs84+"'";
+            telefono = "'" + json[i].phone+"'";
+            email = "'" + json[i].email+"'";
+            descripcion = "'" + json[i].documentDescription+"'";
+            query[i] = "INSERT INTO biblioteca (nombre, tipo, direccion, codigoPostal, longitud, latitud, telefono, email, descripcion, enLocalidad) VALUES(" +
+            nombre + "," + tipo + "," + direccion + "," + codigopostal + "," + longitud + "," + latitud + "," + telefono + "," + email + "," + descripcion + ", NULL);"
+        }
     }
     return query;
 }
